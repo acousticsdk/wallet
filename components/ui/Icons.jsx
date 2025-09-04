cf57d0fc98d799214ebbfcf09eefcd63ec0e81bb
Максим <acoustic.sdk@gmail.com>
2025-09-04 20:08:59 +0300

    Updated Icons.jsx

--- components/ui/Icons.jsx
+++ components/ui/Icons.jsx
@@ -3,15 +3,10 @@ import Svg, { Path, G, Defs, ClipPath, Rect, Mask, Circle, Stop, LinearGradient 
 
 export function BankIcon({ size = 24, color = '#FFFFFF' }) {
   return (
-    <Svg width={size} height={size} viewBox="0 0 24 24" fill="none">
-      <Path 
-        d="M3 21H21M5 21V7L12 3L19 7V21M9 9H15M9 13H15M9 17H15" 
-        stroke={color} 
-        strokeWidth="2" 
-        strokeLinecap="round" 
-        strokeLinejoin="round"
-      />
-    </Svg>
+   <svg width="58" height="65" viewBox="0 0 58 65" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M28.8391 0.835938L0.097168 16.0433V22.1263H57.581V16.0433M42.4537 28.2092V49.4995H51.5301V28.2092M0.097168 64.7069H57.581V55.5825H0.097168M24.3009 28.2092V49.4995H33.3773V28.2092M6.1481 28.2092V49.4995H15.2245V28.2092H6.1481Z" fill="white"/>
+</svg>
+
   );
 }
 
